0ef40756a4c85e30e1ce3e02dec6573926e65dce
laura <laura@salespoints.co>
2021-09-21 20:50:50 +0300

    add function

--- js/accordion.js
+++ js/accordion.js
@@ -1,17 +1,17 @@
 const btns = document.querySelectorAll(".feature__link");
+const btn = document.querySelector(".feature__link");
 const lists = document.querySelectorAll(".feature-sub");
 
 btns.forEach((btnItem, index) => {
   btnItem.addEventListener("click", () => {
-    btns.forEach((btnItem) => {
-      btnItem.classList.remove("feature__link_active");
-    });
-    btnItem.classList.add("feature__link_active");
-
-    lists.forEach((listItem) => {
-        listItem.classList.add('hidden');
-    })
-
-    lists[index].classList.remove('hidden');
+    btns.forEach((btnItem, idx) => {
+      if (btnItem === btn) {
+        btnItem.classList.toggle("feature__link_active");
+        lists[idx].classList.toggle("hidden");
+      } else {
+        btnItem.classList.remove("feature__link_active");
+        lists[idx].classList.add("hidden");
+      }
+    });   
   });
 });
